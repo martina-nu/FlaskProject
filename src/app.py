90932b37b9aae03281cf3bc0c508291e2e9b4335
martina-nu <martinanu98@gmail.com>
2022-08-22 15:56:20 +0000

    Coffee API

--- src/app.py
+++ src/app.py
@@ -1,4 +1,32 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, render_template, jsonify, request 
+import pickle
+import pandas as pd
+import numpy as np
 
-# your code here
+app = Flask(__name__) # hacer ref al nombre del archivo
+
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask'
+
+@app.route('/inicio')
+def show_home():
+    return render_template('Index.html')
+
+@app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>')
+def result(country,variety,aroma,aftertaste,acidity,body,balance,moisture):
+    cols = ['country_of_origin','variety','aroma', 'aftertaste','acidity','body','balance','moisture']
+    data = [country,variety,aroma,aftertaste,acidity,body,balance,moisture]
+    posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
+    loaded_model = pickle.load(open('../models/coffee_model.pkl','rb'))
+    result = loaded_model.predict(posted)
+    text_result = result.tolist()[0]
+
+    if text_result == 'Yes':
+        return jasonify(message='Es un café de especialidad'),200
+    else:
+        return jasonify(message='No es un café de especialidad'),200
+
+if __name__ == '__main__':
+    app.run(debug= True, host='127.0.0.1', port=5000) 
+
